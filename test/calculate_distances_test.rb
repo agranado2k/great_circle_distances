296c3563ddbbdf0532303cd32bb9b80be7659024
Arthur Granado <agranado@pluga.co>
2017-08-02 20:16:51 -0300

    check if poins are less or equals a distance (100)

--- test/calculate_distances_test.rb
+++ test/calculate_distances_test.rb
@@ -2,6 +2,10 @@ require "minitest/autorun"
 
 require "json"
 class CalculateDistances
+  def distance_less_and_equal_than(lat1, lon1, lat2, lon2, distance)
+    great_circle_distance(lat1, lon1, lat2, lon2) <= distance
+  end
+
   def great_circle_distance(lat1, lon1, lat2, lon2)
     lat1_in_radians = to_radians(lat1)
     lat2_in_radians = to_radians(lat2)
@@ -44,6 +48,22 @@ class CalculateDistancesTest < Minitest::Test
     distance = 138.01
 
     assert_equal distance, @cd.great_circle_distance(lat1, lon1, lat2, lon2)
+  end
+
+  def test_distance_less_and_equal_than_100_km
+    lat1, lon1 = 53.3393,-6.2576841
+    lat2, lon2 = 52.986375,-6.043701
+    km_100 = 100
+
+    assert @cd.distance_less_and_equal_than(lat1, lon1, lat2, lon2, km_100)
+  end
+
+  def test_distance_greater_than_100_km
+    lat1, lon1 = 53.3393,-6.2576841
+    lat2, lon2 = 51.92893,-10.27699
+    km_100 = 100
+
+    refute @cd.distance_less_and_equal_than(lat1, lon1, lat2, lon2, km_100)
   end
 
 end
